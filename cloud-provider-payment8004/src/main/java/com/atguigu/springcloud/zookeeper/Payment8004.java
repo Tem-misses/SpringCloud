d92affd18afd1bd4418aff520e99acaf6d86fffd
jxtem <107570398@qq.com>
2021-10-10 13:15:34 +0800

    first

---
+++ cloud-provider-payment8004/src/main/java/com/atguigu/springcloud/zookeeper/Payment8004.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud.zookeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author: Tem_misses
+ * @Description:
+ * @DateTime: 2021/10/8 13:18
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Payment8004 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Payment8004.class, args);
+    }
+
+}
